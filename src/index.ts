912d4fbe91a138563bc6c0c1f07fdf6f6959bdde
kamihatex <plutoismycousin@gmail.com>
2025-11-23 17:51:31 +0200

    set up db connections and schema and populated db with demo user

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { app } from './server.ts'
 import { env } from '../env.ts'
 
 app.listen(env.PORT, () => {
-  console.log(`server runnign on port: ${env.PORT}`)
+  console.log(`server running on port: ${env.PORT}`)
 })
