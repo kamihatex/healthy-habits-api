824bde92edde0716f0c77f08a2f5f161eae73a8c
kamihatex <plutoismycousin@gmail.com>
2025-11-23 15:56:10 +0200

    introducing routes and middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
+import { app } from './server.ts'
 import { env } from '../env.ts'
-import app from './server.ts'
 
 app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT}`)
-  console.log(`Environment: ${env.APP_STAGE}`)
+  console.log(`server runnign on port: ${env.PORT}`)
 })
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.ts'
+import userRoutes from './routes/userRoutes.ts'
+import habitRoutes from './routes/habitRoutes.ts'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { isTest } from '../env.ts'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  })
+)
 
 app.get('/health', (req, res) => {
-  res.status(200).json({
-    status: 'OK',
-    timestamp: new Date().toISOString(),
-    service: 'Habit Tracker API',
-  })
+  res.send('<button>another</button>')
 })
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/habits', habitRoutes)
 
 export { app }
 
